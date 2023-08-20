1332bbe1cbc11ae343af29729f93e5a18ddc5c71
raykeat <teohraykeat@gmail.com>
2023-08-21 01:22:13 +0800

    Exercise Tracker with MERN Tech Stack

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import EditExercise from "./components/edit-exercises.component";
+import ExercisesList from "./components/exercises-list.component";
+import SpecificExercise from "./components/specific-exercise.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <div className = "container">
+        <Navbar />
+        {/* self closing html tag that represents line break */}
+        <br/>
+
+        <Routes>
+          {/*Route maps specific URL paths to different React Components*/}
+          <Route path = "/" exact element={<ExercisesList />} />
+          <Route path = "/create" element={<CreateExercise />} />
+          <Route path = "/user" element={<CreateUser />} />
+          <Route path = "/editpost" element={<EditExercise />} />
+          <Route path = "/specificpost/editpost" element={<EditExercise />} />
+          <Route path = "/specificpost" element={<SpecificExercise />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
