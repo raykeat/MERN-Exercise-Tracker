07e1e6c91cc06b2162fc19f62a65f1e74e1dd230
raykeat <116874025+raykeat@users.noreply.github.com>
2023-09-11 00:09:00 +0800

    Update create-user.component.js

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -64,7 +64,7 @@ export default class CreateUser extends Component {
         })
 
         //http://localhost:5000/
-        axios.post('https://mern-exercise-tracker-ten.vercel.app/users/add', user)
+        axios.post('https://mern-exercise-tracker-five.vercel.app/users/add', user)
             .then(res => console.log(res.data));
     }
 
