39b34113e8e9ff0a468ecf29c289e0821ff3ab4c
raykeat <116874025+raykeat@users.noreply.github.com>
2023-09-12 15:11:10 +0800

    Update edit-exercises.component.js

--- src/components/edit-exercises.component.js
+++ src/components/edit-exercises.component.js
@@ -83,7 +83,7 @@ function EditExercise () {
 
         //making axios post request to backend EXPRESS endpoints/routes, which then performs CRUD operations in MongoDB
         //http://localhost:5000/
-        axios.post(`https://mern-exercise-tracker-five.vercel.app/exercises/update/${exercise.exerciseid}`,exercise)
+        axios.post(`https://mern-exercisetracker-854091fc1e0c.herokuapp.com/exercises/update/${exercise.exerciseid}`, exercise)
             .then(res => {
                 console.log(res.data);
                 setEdited(true);
